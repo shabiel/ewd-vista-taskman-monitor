da8ca9b178ce3f2c488e708e2ee15a8074931c81
Alexis Carlson <whatisthehumanspirit@gmail.com>
2016-12-26 15:00:30 -0800

    First simple test call.

--- client/vista-taskman-monitor.js
+++ client/vista-taskman-monitor.js
@@ -1,5 +1,16 @@
 let clientMethods = {};
 
-
+clientMethods.showTasks = function(EWD) {
+	let messageObj = {
+		service: 'ewd-vista-taskman-monitor',
+	    type: 'tasks'
+	};
+	EWD.send(messageObj, function(responseObj) {
+	    var tasks = responseObj.message.tasks;
+	    
+	    console.log("Tasks:");
+	    console.log(tasks);
+	}
+}
 
 module.exports = clientMethods;
