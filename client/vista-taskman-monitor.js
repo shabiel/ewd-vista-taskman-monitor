e73a3cfbdbde0858a881f619f18665cef02fc53d
Alexis Carlson <whatisthehumanspirit@gmail.com>
2016-12-27 14:37:54 -0800

    Updates.

--- client/vista-taskman-monitor.js
+++ client/vista-taskman-monitor.js
@@ -1,16 +1,15 @@
 let clientMethods = {};
 
 clientMethods.showTasks = function(EWD) {
-	let messageObj = {
-		service: 'ewd-vista-taskman-monitor',
-	    type: 'tasks'
-	}
-	EWD.send(messageObj, function(responseObj) {
-	    var tasks = responseObj.message.tasks;
-	    
-	    console.log("Tasks:");
-	    console.log(tasks);
-	});
+    let messageObj = {
+        service: 'ewd-vista-taskman-monitor',
+        type: 'tasks'
+    }
+    EWD.send(messageObj, function(responseObj) {
+        let tasks = responseObj.message.tasks;
+
+        console.log(tasks);
+    });
 }
 
 module.exports = clientMethods;
